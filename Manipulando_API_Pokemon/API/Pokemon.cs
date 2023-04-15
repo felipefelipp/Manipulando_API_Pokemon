babf006938fd57d11cec71a06b581fde21e2f9f8
felipeSantosf <lipesantos.gtt@gmail.com>
2023-04-15 07:56:21 -0300

    Adicionar arquivos de projeto.

---
+++ Manipulando_API_Pokemon/API/Pokemon.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manipulando_API_Pokemon.API
+{
+    public  class Pokemon
+    { 
+        public void BuscarPokemon(string nome)
+        {
+            var client = new RestClient($"https://pokeapi.co/api/v2/"); // api
+            RestRequest request = new RestRequest($"pokemon/{nome}", Method.Get); // rota
+            var response = client.Execute(request);
+
+            if(response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                var pokemon = JsonConvert.DeserializeObject(response.Content);
+                Console.WriteLine(JsonConvert.SerializeObject(pokemon, Formatting.Indented));    
+            } else
+            {
+                Console.WriteLine(response.ErrorMessage);
+            }
+            Console.ReadKey();
+        }
+
+    }
+} 
---
+++ Manipulando_API_Pokemon/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+using Manipulando_API_Pokemon.API;
+
+var nome = "blastoise";
+
+Pokemon pokemon = new Pokemon();
+pokemon.BuscarPokemon(nome);
